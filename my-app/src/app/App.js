2648685a8751d2d7ca03fd2d97ef1066c379479d
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-03-21 20:48:35 +0300

    Add style calculate

--- my-app/src/app/App.js
+++ my-app/src/app/App.js
@@ -4,9 +4,10 @@ import Calculate from "../components/calculate/Calculate";
 
 function App() {
   return (
-    <>
+    <div>
+        <h1 style={{textAlign: "center"}}>Калькулятор!</h1>
         <Calculate/>
-    </>
+    </div>
   );
 }
 
--- my-app/src/components/calculate/Calculate.js
+++ my-app/src/components/calculate/Calculate.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
+import "./calculateStyle.css"
 
 function Calculate() {
-    const [outputResult, setOutputResult] = useState('')
+    const [outputResult, setOutputResult] = useState("")
 
     const handleClick = (event) => {
         const value = event.target.name
@@ -17,26 +18,26 @@ function Calculate() {
     }
 
     return (
-        <div>
-            <h3 style={{textAlign: "center"}}>{outputResult}</h3>
-            <button name="0" onClick={handleClick}>0</button>
-            <button name="1" onClick={handleClick}>1</button>
-            <button name="2" onClick={handleClick}>2</button>
-            <button name="3" onClick={handleClick}>3</button>
-            <button name="4" onClick={handleClick}>4</button>
-            <button name="5" onClick={handleClick}>5</button>
-            <button name="6" onClick={handleClick}>6</button>
-            <button name="7" onClick={handleClick}>7</button>
-            <button name="8" onClick={handleClick}>8</button>
-            <button name="9" onClick={handleClick}>9</button>
-            <button name="+" onClick={handleClick}>+</button>
-            <button name="-" onClick={handleClick}>-</button>
-            <button name="*" onClick={handleClick}>*</button>
-            <button name="/" onClick={handleClick}>/</button>
-            <button name="=" onClick={calculationResult}>=</button>
-            <button name="C" onClick={clearResult}>C</button>
-
-
+        <div style={{paddingLeft: "500px", paddingRight: "500px"}}>
+            <h3 className="result">{outputResult}</h3>
+            <div className="place-button-calculate">
+                <button className="button" name="0" onClick={handleClick}>0</button>
+                <button className="button" name="1" onClick={handleClick}>1</button>
+                <button className="button" name="2" onClick={handleClick}>2</button>
+                <button className="button" name="3" onClick={handleClick}>3</button>
+                <button className="button" name="4" onClick={handleClick}>4</button>
+                <button className="button" name="5" onClick={handleClick}>5</button>
+                <button className="button" name="6" onClick={handleClick}>6</button>
+                <button className="button" name="7" onClick={handleClick}>7</button>
+                <button className="button" name="8" onClick={handleClick}>8</button>
+                <button className="button" name="9" onClick={handleClick}>9</button>
+                <button className="button" name="+" onClick={handleClick}>+</button>
+                <button className="button" name="-" onClick={handleClick}>-</button>
+                <button className="button" name="*" onClick={handleClick}>*</button>
+                <button className="button" name="/" onClick={handleClick}>/</button>
+                <button className="button" name="=" onClick={calculationResult}>=</button>
+                <button className="button" name="C" onClick={clearResult}>C</button>
+            </div>
         </div>
     )
 }
