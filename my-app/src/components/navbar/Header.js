66b5d6443519caff9c98fab3e6a82ea70798304d
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-03-28 17:38:51 +0300

    Global Refactor, Developed Weather

--- my-app/src/components/navbar/Header.js
+++ my-app/src/components/navbar/Header.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Navbar, Nav} from "react-bootstrap"
-import iconCalculate from "../../images/iconCalculate.png"
-import iconWeather from "../../images/temperature-list-svgrepo-com.svg"
+import iconCalculate from "../../images/calculator-svgrepo-com (2).svg"
+import iconWeather from "../../images/weather-svgrepo-com (4).svg"
 import "./Header.css"
 
 // Иконку погоды обязательно заменить
 
 function Header() {
     return (
-        <Navbar bg="light">
-            <Navbar.Brand className="mr-auto" href="/">Recipe&Calculate</Navbar.Brand>
+        <Navbar style={{backgroundColor: "white"}}>
+            <Navbar.Brand className="mr-auto" href="/"><span className="brand-style">Weather&Calculate</span></Navbar.Brand>
             <Nav className="d-flex justify-content-between w-10">
                 <Nav.Link href="/calculate" className="zoom">
                     <img
--- my-app/src/components/weather/Weather.js
+++ my-app/src/components/weather/Weather.js
@@ -4,7 +4,16 @@ import Form from "react-bootstrap/Form"
 import Card from "react-bootstrap/Card"
 import "../weather/Weather.css"
 import iconMagnifer from "../../images/satellite-dish-svgrepo-com.svg"
-import weatherIcon from "../../images/clouds-svgrepo-com.svg"
+import iconCloud from "../../images/iconForWeather/cloud.svg"
+import iconRain from "../../images/iconForWeather/rain.svg"
+import iconClear from "../../images/iconForWeather/sun.svg"
+import iconSnow from "../../images/iconForWeather/snow.svg"
+import iconSmallRain from "../../images/iconForWeather/smallRain.svg"
+import iconMinusThermometer from "../../images/iconForWeather/iconMinusThermometer.svg"
+import iconPlusThermometer from "../../images/iconForWeather/thermometerPlus.svg"
+import iconCloudyWithClarifications from "../../images/iconForWeather/cloudyWithClarifications.svg"
+import iconWind from "../../images/iconForWeather/windVersTwo.svg"
+import iconHumidity from "../../images/iconForWeather/humidity.svg"
 
 function Weather() {
     const [inputValue, setInputValue] = useState("")
@@ -31,15 +40,125 @@ function Weather() {
         setInputValue(event.target.value)
     }
 
-    // Нужно придумать алгоритм создания иконок при различной погоде
+    const iconForWeather = (numDay) => {
+        switch (weather.list[numDay].weather[0].description) {
+            case "пасмурно":
+                return (
+                    <img
+                        alt="cloud"
+                        src={iconCloud}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case "облачно с прояснениями":
+                return (
+                    <img
+                        alt="cloudy"
+                        src={iconCloudyWithClarifications}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case "переменная облачность":
+                return (
+                    <img
+                        alt="cloudy"
+                        src={iconCloudyWithClarifications}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case "небольшая облачность":
+                return (
+                    <img
+                        alt="cloudy"
+                        src={iconCloudyWithClarifications}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case "дождь":
+                return (
+                    <img
+                        alt="rain"
+                        src={iconRain}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case "небольшой дождь":
+                return (
+                    <img
+                        alt="smallRain"
+                        src={iconSmallRain}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case "ясно":
+                return (
+                    <img
+                        alt="light"
+                        src={iconClear}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case "небольшой снег":
+                return (
+                    <img
+                        alt="smallSnow"
+                        src={iconSnow}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case "снег":
+                return (
+                    <img
+                        alt="snow"
+                        src={iconSnow}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+        }
+    }
+
+    const iconThermometer = (numDay) => {
+        const temperature = weather.list[numDay].main.temp;
+
+        switch (temperature > 0) {
+            case true:
+                return (
+                    <img
+                        alt="thermometerPlus"
+                        src={iconPlusThermometer}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+            case false:
+                return (
+                    <img
+                        alt="thermometerMinus"
+                        src={iconMinusThermometer}
+                        height="30px"
+                        width="30px"
+                    />
+                )
+        }
+    }
 
     return (
         <div>
             <div className="form-style">
                 <Form>
-                    <Form.Control onChange={handleChange} type="text" value={inputValue} placeholder="Город или село"/>
+                    <Form.Control onChange={handleChange} type="text" value={inputValue}
+                                  placeholder="Город или регион"/>
                 </Form>
-                <Button variant="outline-primary" className="button-style" onClick={searchPressed}>
+                <Button variant="primary" className="button-style" onClick={searchPressed}>
                     <img
                         alt={"icon"}
                         src={iconMagnifer}
@@ -48,28 +167,162 @@ function Weather() {
                     />
                 </Button>
             </div>
-            <div>
+            <div style={{paddingLeft: "50px"}}>
                 {visible &&
-                    <Card className="w-25">
-                        <Card.Header>{visible && weather.city.name}{"\n"}{visible && <span style={{fontSize: "13px"}}>
-                        {weather.list["5"].dt_txt}
+                    <Card className="w-25" style={{boxShadow: "0px 0px 5px 0px #161616"}}>
+                        <Card.Header>{visible && weather.city.name}{"\n"}{visible &&
+                            <span> сегодня: </span>}</Card.Header>
+                        <Card.Body>
+                            <div>
+                                <Card.Text>
+                                    Температура {iconThermometer("2")} {visible && weather.list["2"].main.temp + "°C"}
+                                </Card.Text>
+                                <Card.Text>
+                                    Погода {iconForWeather("2")}
+                                </Card.Text>
+                                <Card.Text>
+                                    Скорость ветра
+                                    <img
+                                        alt="wind"
+                                        src={iconWind}
+                                        height="30px"
+                                        width="30px"
+                                    />
+                                    {weather.list["2"].wind.speed}м/c
+                                </Card.Text>
+                                <Card.Text>
+                                    Влажность
+                                    <img
+                                        alt="humidity"
+                                        src={iconHumidity}
+                                        height="30px"
+                                        width="30px"
+                                    />
+                                    {weather.list["2"].main.humidity + "%"}
+                                </Card.Text>
+                            </div>
+                    </Card.Body>
+
+                    </Card>
+                }
+            </div>
+            <div className="card-icon-position">
+                {visible &&
+                    <Card className="w-20" style={{boxShadow: "0px 0px 5px 0px #161616"}}>
+                        <Card.Header>{visible && <span
+                            style={{fontSize: "16px", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                        {weather.list["12"].dt_txt}
                     </span>}</Card.Header>
                         <Card.Body>
                             <Card.Text>
-                                {visible && weather.list["5"].main.temp + "°C"}
+                                {iconThermometer("12")}
+                                {visible && weather.list["12"].main.temp + "°C"}
                             </Card.Text>
                             <Card.Text>
-                                {visible && weather.list["5"].weather[0].main}{' '}
+                                {iconForWeather("12")}
+                            </Card.Text>
+                            <Card.Text>
                                 <img
-                                    alt="weatherIcon"
-                                    src={weatherIcon}
-                                    width="30px"
+                                    alt="thermometer"
+                                    src={iconWind}
                                     height="30px"
+                                    width="30px"
                                 />
+                                {weather.list["12"].wind.speed}м/c
                             </Card.Text>
                         </Card.Body>
                     </Card>
                 }
+                <div className="card-pos">
+                    {visible &&
+                        <Card className="w-20">
+                            <Card.Header>{visible && <span style={{
+                                fontSize: "16px",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}>
+                        {weather.list["20"].dt_txt}
+                    </span>}</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {iconThermometer("20")} {visible && weather.list["20"].main.temp + "°C"}
+                                </Card.Text>
+                                <Card.Text>
+                                    {iconForWeather("20")}
+                                </Card.Text>
+                                <Card.Text>
+                                    <img
+                                        alt="thermometer"
+                                        src={iconWind}
+                                        height="30px"
+                                        width="30px"
+                                    /> {weather.list["20"].wind.speed}м/c
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    }
+                </div>
+                <div className="card-pos">
+                    {visible &&
+                        <Card className="w-20">
+                            <Card.Header>{visible && <span style={{
+                                fontSize: "16px",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}>
+                        {weather.list["28"].dt_txt}
+                    </span>}</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {iconThermometer("28")} {visible && weather.list["28"].main.temp + "°C"}
+                                </Card.Text>
+                                <Card.Text>
+                                    {iconForWeather("28")}
+                                </Card.Text>
+                                <Card.Text>
+                                    <img
+                                        alt="thermometer"
+                                        src={iconWind}
+                                        height="30px"
+                                        width="30px"
+                                    /> {weather.list["28"].wind.speed}м/c
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    }
+                </div>
+                <div className="card-pos">
+                    {visible &&
+                        <Card className="w-20">
+                            <Card.Header>{visible && <span style={{
+                                fontSize: "16px",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}>
+                        {weather.list["36"].dt_txt}
+                    </span>}</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {iconThermometer("36")} {visible && weather.list["36"].main.temp + "°C"}
+                                </Card.Text>
+                                <Card.Text>
+                                    {iconForWeather("36")}
+                                </Card.Text>
+                                <Card.Text>
+                                    <img
+                                        alt="thermometer"
+                                        src={iconWind}
+                                        height="30px"
+                                        width="30px"
+                                    /> {weather.list["36"].wind.speed}м/c
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    }
+                </div>
             </div>
         </div>
     );
